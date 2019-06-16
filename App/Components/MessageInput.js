d5ad24021ba27faacced6fce83a577967657b0a7
steven <stvtortora@gmail.com>
2019-06-16 15:03:23 -0400

    add text input auto grow

--- App/Components/MessageInput.js
+++ App/Components/MessageInput.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TextInput, Button, View, StyleSheet } from 'react-native'
+import { Button, View, StyleSheet, TextInput } from 'react-native'
 import { connect } from 'react-redux'
 
 class MessageInput extends React.Component {
@@ -7,26 +7,42 @@ class MessageInput extends React.Component {
     super(props)
     this.state = {
       message: '',
-      user_email: this.props.user_email
+      user_email: this.props.user_email,
+      height: 50
     }
     this.addMessage = this.addMessage.bind(this)
+    this.growInput = this.growInput.bind(this)
     this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange (text) {
-    this.setState({
-      message: text
-    })
   }
 
   addMessage () {
     this.props.addMessage(this.state)
   }
 
+  growInput () {
+    return (e) => {
+      this.handleChange('height')(e.nativeEvent.contentSize.height)
+    }
+  }
+
+  handleChange (field) {
+    return (value) => {
+      this.setState({
+        [field]: value
+      })
+    }
+  }
+
   render () {
     return (
       <View style={styles.viewContainer}>
-        <TextInput style={styles.textInput} onChangeText={this.handleChange} placeholder='Enter a message...'></TextInput>
+        <TextInput
+        style={{height: this.state.height, width: '80%'}}
+        placeholder='Enter a message...'
+        editable={true}
+        multiline={true}
+        onChangeText={this.handleChange('message')}
+        onContentSizeChange={this.growInput}/>
         <Button style={styles.sendButton} onPress={this.addMessage} title='Send'></Button>
       </View>
     )
@@ -37,9 +53,6 @@ const styles = StyleSheet.create({
   viewContainer: {
     flex: .2, flexDirection: 'row', justifyContent: 'space-around',
     alignItems: 'center', marginLeft: 35, marginRight: 35, marginBottom: 35, height: 50
-  },
-  textInput: {
-    height: 50, width: '80%'
   }
 })
 
